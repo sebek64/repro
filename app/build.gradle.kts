7890de9032de11e865ca67b473dcb61e2a7ca046
Marcel Sebek <marcel.sebek@deutsche-boerse.com>
2023-03-21 10:36:56 +0100

    Add repro case

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     application
+    distribution
 }
 
 repositories {
@@ -7,8 +8,16 @@ repositories {
 }
 
 dependencies {
+    implementation("com.rabbitmq:amqp-client:5.16.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
 }
 
 application {
     mainClass.set("repro.App")
 }
+
+task<Exec>("buildDockerImage") {
+    commandLine("docker", "build", "--rm", "-t", "repro:1.0.0", ".")
+}
+
+tasks.build { dependsOn("buildDockerImage") }
